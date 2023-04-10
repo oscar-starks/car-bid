8706a6ab2b19d96c7a1204a1e1aadbdcd0f2e61b
oscar-starks <oscarblessed04@gmail.com>
2023-04-10 13:23:31 +0100

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from accounts.managers import CustomUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+import uuid
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+GENDER_CHOICES = (
+    ("male", "male"),
+    ("female", "female"),
+    
+)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = None
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    first_name = models.CharField(verbose_name=(_("First name")),blank = True, null = True, max_length=300)
+    last_name = models.CharField(verbose_name=(_("Last name")),blank = True, null = True, max_length=300)
+    avatar = models.ImageField(upload_to="avatar/", blank=True)
+    email = models.EmailField(verbose_name=(_("Email address")), unique=True)
+    phone_number = models.CharField(max_length=45, unique=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    gender = models.CharField(choices= GENDER_CHOICES, max_length= 10, blank = True, null = True)
+    address = models.CharField(blank = True, null = True, max_length= 100)
+    vat_no = models.CharField(max_length=1000)
+    hrb_no = models.CharField(max_length=1000,blank = True, null = True)
+    company_reg_no = models.CharField(max_length=1000)
+    tax_id_number = models.CharField(max_length=1000)
+    identification = models.FileField(upload_to="identification/")
+    dealer = models.BooleanField(default=False)
+    seller = models.BooleanField(default=False)
+
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return str(self.email)
