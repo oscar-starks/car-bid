2caf3fd5757d1fc9b069b062ce7435140c367fd2
oscar-starks <oscarblessed04@gmail.com>
2023-04-22 08:59:42 +0100

    offer endpoint implemented

--- bidding/urls.py
+++ bidding/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('auction/', AuctionView.as_view()),
     path('car/', CarDetailView.as_view),
 
-    path("make_offer/",BidOfferView.as_view()),
+    path("make_offer/<str:car_id>/",BidOfferView.as_view()),
 ]
